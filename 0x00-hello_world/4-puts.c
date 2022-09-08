2d3db4eb604e6f572a6b4e4d87c8782221697a31
Funmilayo <adegbilefunmilayo@gmail.com>
2022-09-08 14:26:03 -0700

    c programming task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <studio.h>
+/**
+ * main - Entry point
+  *
+  *Return: 0 after printingthe function
+  */
+int main(void)
+{puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
