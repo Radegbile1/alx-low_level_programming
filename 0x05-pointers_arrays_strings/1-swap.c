d3b7d48c333a1279d1709c82207f648b03a234b0
Funmilayo <adegbilefunmilayo@gmail.com>
2022-09-19 04:05:43 -0700

    task2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swap two integers value
+ * @a: first integer
+ * @b: snd integer
+ * Return: returns nothing
+ */
+
+void swap_int(int *a, int*b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
