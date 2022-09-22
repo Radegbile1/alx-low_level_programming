61819dfa27436923fd7c443fe0e14edf8b44757f
Funmilayo <adegbilefunmilayo@gmail.com>
2022-09-22 06:19:21 -0700

    task3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - check if a character is lowercase if YES returns 1
+ * otherwise returns 0
+ * Return: always 0 if it is a lowercase
+ */
+
+int _islower(int c) 
+{
+	if (c >= 97 && c<= 122)
+		return (1);
+	else
+		return (0);
+}
