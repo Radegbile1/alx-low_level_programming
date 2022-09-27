6a6e8fed5db032a51637ea6d81c20a66f06074f4
Funmilayo <adegbilefunmilayo@gmail.com>
2022-09-27 05:20:41 -0700

    task0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s - memory to be filled
+ * @b - character to copy
+ * @n - numbers of byte to be used
+ */
+
+char *_memset(char *s, char b, unsigned int)
+{
+
+	unsigned int i;
+
+	for (i = 0, i < n, i++)
+{
+	s[i] = b;
+}
+
+return (s);
+}
